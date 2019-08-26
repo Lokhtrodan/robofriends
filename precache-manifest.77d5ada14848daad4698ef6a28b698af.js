8bff89edc747ce0402a1f2fcd095e5be61d7f488
Lokhtrodan <slayerenkala@gmail.com>
2019-08-26 03:59:36 +0200

    Updates

---
+++ precache-manifest.77d5ada14848daad4698ef6a28b698af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f1473c319ab9aa735bcfb5970da7efe",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b4a8b64b7033b8b4b563",
+    "url": "/robofriends/static/css/main.72715790.chunk.css"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/robofriends/static/js/2.80c069d3.chunk.js"
+  },
+  {
+    "revision": "b4a8b64b7033b8b4b563",
+    "url": "/robofriends/static/js/main.52a1c56e.chunk.js"
+  },
+  {
+    "revision": "8bd552311f5d528a5605",
+    "url": "/robofriends/static/js/runtime~main.e3d5a5e7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
